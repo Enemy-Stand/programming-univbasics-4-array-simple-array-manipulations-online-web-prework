8bfcb0a7a3207ae1614060892ba582e6e1c7ae2f
Joseph Sesti <sestijoey63@gmail.com>
2019-08-07 23:26:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,5 +31,5 @@ end
 def using_concat(array)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
-  (my_favorite_things, more_favs)
+  my_favorite_things.concatavs)
 end
