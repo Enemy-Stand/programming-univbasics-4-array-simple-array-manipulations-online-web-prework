e00ba46a5abbe80718a6a3c0eff23615142053bb
Joseph Sesti <sestijoey63@gmail.com>
2019-08-07 22:51:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,7 +5,7 @@ end
 
 def using_unshift(array, element)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  bouroughs_in_nyc.unshift(element)
+  array.unshift(element)
 end
 
 def using_pop(array)
