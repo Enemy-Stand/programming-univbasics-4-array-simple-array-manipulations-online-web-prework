e5485c8cc337789eb7d4fb5c0ba0d5f138a4f82a
Joseph Sesti <sestijoey63@gmail.com>
2019-08-07 23:17:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,4 +29,6 @@ def shift_with_args(array)
 end
 
 def using_concat(array)
-  
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  array = using_concat(my_favorite_things, more_favs)
