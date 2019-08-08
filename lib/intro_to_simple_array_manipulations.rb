97c044f6ac4802aec91cef6b81bf79694003d3d4
Joseph Sesti <sestijoey63@gmail.com>
2019-08-08 00:23:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -49,5 +49,7 @@ def using_flatten(array)
   array.flatten
 end
 
-def using_delete
-  
+def using_delete(array, string)
+  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+  array.delete(string)
+end
